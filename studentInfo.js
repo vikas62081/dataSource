633f93aa05986bf421bfc251ee1cfab4f6c4736d
vikas62081 <vikas62081@gmail.com>
2020-10-01 21:22:20 +0530

    adding student info

---
+++ studentInfo.js
@@ -0,0 +1,4 @@
+[
+    { name: "Rahul", age: 19, phoneNumber: 9876543210, birthYear:2001}, 
+    { name: "David", age: 17, phoneNumber: 9827654310,birthYear:2003}, 
+    { name: "Dan", age: 25, phoneNumber: 9765438210,birthYear:1995 }]
